d4232b629bebdf505f5c2aca753ac6a11d842575
Kei Nishina <k247loveke@gmail.com>
2015-10-01 22:33:32 +0859

    passed test_name

--- sherry.rb
+++ sherry.rb
@@ -1,11 +1,50 @@
 #! ruby -Ks
 
-class Sherry
-   def initialize
-     puts "Hello, world"
+class Responder
+  def initialize( name )
+    @name = name
+  end
+
+   def name
+     return @name
    end
 end
 
-Sherry.new
+class Sherry
+   def initialize( name )
+     @name = name
+     @responder = Responder.new('What')
+   end
+
+   def name
+     return @name
+   end
+end
+
+if $0 == __FILE__ 
+  require 'minitest/autorun'
+
+  class TestResponder < MiniTest::Unit::TestCase
+    def setup
+      @responder = Responder.new('What')
+    end
+
+    def test_name
+      assert_equal 'What', @responder.name
+    end
+  end
+
+  class TestSherry < MiniTest::Unit::TestCase
+    def setup
+      @sherry = Sherry.new( "Sakura" )
+    end
+
+    def test_name
+      assert_equal "Sakura", @sherry.name
+    end
+
+  end
+end
+
 
 __END__
